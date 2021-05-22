ff9ddacb2f243d0a5f353602cd855677134bc55b
Yann8313 <yannick.ayassa@gmail.com>
2021-05-22 17:43:16 -0400

    first commit

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,15 @@
+import classes from "./MealsSummary.module.scss"
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious food,Delivered To You</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur blanditiis, corporis enim hic
+                illo impedit, magni obcaecati quaerat recusandae, reprehenderit sequi vero. Dignissimos dolore doloribus
+                inventore sequi tempora unde?</p>
+
+        </section>
+    );
+};
+
+export default MealsSummary;
